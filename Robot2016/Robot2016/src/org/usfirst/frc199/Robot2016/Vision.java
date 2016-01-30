d9c8c77fdc0cb315eed3870242fa44b2da4a7d4b
a-zuckut <zuckutalex@yahoo.com>
2016-01-30 13:48:48 -0800

    Vision start of OpenCV?

--- Robot2016/Robot2016/src/org/usfirst/frc199/Robot2016/Vision.java
+++ Robot2016/Robot2016/src/org/usfirst/frc199/Robot2016/Vision.java
@@ -1,22 +1,35 @@
 package org.usfirst.frc199.Robot2016;
 
-import edu.wpi.first.wpilibj.vision.USBCamera;
-import com.ni.vision.NIVision;
+import org.opencv.core.Mat;
+import org.opencv.videoio.VideoCapture;
 
 public class Vision {
 
-	USBCamera cam;
-	NIVision x;
-	
-	public Vision(){
+	String urlOfCamera = "http://10.1.0.11";
+
+	VideoCapture camera;
+	Mat frame = new Mat();
+
+	public Vision() {
+		try {
+			System.out.println("It started the camera constructor.");
+			camera = new VideoCapture(urlOfCamera);
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+		}
 	}
-	
+
+	public void initCamera() {
+		camera.read(frame);
+		System.out.println("Frame Obtained");
+	}
+
 	/**
 	 * did the target enter the camera's vision?
 	 * 
 	 * @return answer to last question
 	 */
-	public boolean isTargetInVisionOfRobotCamera(){
+	public boolean isTargetInVisionOfRobotCamera() {
 		return false;
 	}
 
@@ -25,18 +38,18 @@ public class Vision {
 	 * 
 	 * @return ready to shoot?
 	 */
-	public boolean isRobotDirectlyFacingTarget(){
+	public boolean isRobotDirectlyFacingTarget() {
 		return false;
 	}
-	
+
 	/**
-	 * Just has to a rough estimate: RIGHT: negative LEFT: positive in relation to our current position
+	 * Just has to a rough estimate: RIGHT: negative LEFT: positive in relation
+	 * to our current position
 	 * 
 	 * @return degrees
 	 */
-	public double degreeToTarget(){
+	public double degreeToTarget() {
 		return 0;
 	}
-	
-	
+
 }
