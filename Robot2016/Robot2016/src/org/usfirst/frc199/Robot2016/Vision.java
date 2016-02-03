554f75eb7657a87b8923f01b0c13f9fd07573989
a-zuckut <zuckutalex@yahoo.com>
2016-02-02 17:09:10 -0800

    Vision grip start

--- Robot2016/Robot2016/src/org/usfirst/frc199/Robot2016/Vision.java
+++ Robot2016/Robot2016/src/org/usfirst/frc199/Robot2016/Vision.java
@@ -1,21 +1,66 @@
 package org.usfirst.frc199.Robot2016;
 
+import java.io.IOException;
+
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
 
 public class Vision {
 
-	NetworkTable table;
-
+	// Reference https://github.com/WPIRoboticsProjects/GRIP/wiki/Tutorial:-Run-GRIP-from-a-CPP-or-Java-FRC-program
+	// To learn about how to run GRIP in java programs.
+	
 	private final static String GRIP_CMD = "/usr/local/frc/JRE/bin/java -jar /home/lvuser/grip.jar /home/lvuser/FindingTargets.grip";
+	NetworkTable table = NetworkTable.getTable("GRIP/myContoursReport");
 
 	final String UrlOfCamera = "http://10.1.0.11";
 	final int CannyDefaultThreshold = 50;
 
 	public Vision() {
-		table = NetworkTable.getTable("GRIP/myContourReport");
+		try {
+            new ProcessBuilder(GRIP_CMD).inheritIO().start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 	}
 
 	public void initCamera() {
+		
+	}
+	
+	/**	
+	 * 
+	 * Given the arrays centerX and centerY from the network tables.
+	 * Will return the x, y coordinates that has the max width 
+	 * in which we want to shoot.
+	 * 
+	 * @return
+	 */
+	public double[] findMidXYCoords(){
+		double[] x = new double[0];
+		table.getNumberArray("centerX", x);
+		double[] y = new double[0];
+		table.getNumberArray("centerY", y);
+		double[] width = new double[0];
+		table.getNumberArray("centerY", width);
+		int index = maxIndex(width);
+		return new double[]{x[index], y[index]};
+	}
+	
+	/**
+	 * returns index at which it has max double value in array input
+	 * @param input
+	 * @return
+	 */
+	private int maxIndex(double[] input){
+		double max = 0;
+		int index = 0;
+		for(int i = 0; i < input.length; i++){
+			if(input[i] > max){
+				max = input[i];
+				index = i;
+			}
+		}
+		return index;
 	}
 
 	/**
