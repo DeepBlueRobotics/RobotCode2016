fe30e4abce74075c002a3cdef299f4934a049baa
a-zuckut <zuckutalex@yahoo.com>
2016-02-05 16:47:43 -0800

    Vision Changes

--- Robot2016/Robot2016/src/org/usfirst/frc199/Robot2016/Vision.java
+++ Robot2016/Robot2016/src/org/usfirst/frc199/Robot2016/Vision.java
@@ -1,74 +1,103 @@
 package org.usfirst.frc199.Robot2016;
 
-import edu.wpi.first.wpilibj.networktables.NetworkTable;
+import com.ni.vision.NIVision;
+import com.ni.vision.NIVision.Image;
+import com.ni.vision.NIVision.ImageType;
+
+import edu.wpi.first.wpilibj.CameraServer;
 
 public class Vision {
 
-	// Reference https://github.com/WPIRoboticsProjects/GRIP/wiki/Tutorial:-Run-GRIP-from-a-CPP-or-Java-FRC-program
-	// To learn about how to run GRIP in java programs.
-	
-	private final static String GRIP_CMD = "/usr/local/frc/JRE/bin/java -jar /home/lvuser/grip.jar /home/lvuser/FindingTargets.grip";
-	NetworkTable table = NetworkTable.getTable("GRIP/myContoursReport");
+	int session;
+	Image frame;
+	Image binaryFrame;
 
-	final String UrlOfCamera = "http://10.1.0.11";
-	final int CannyDefaultThreshold = 50;
-	
-	ProcessBuilder builder = new ProcessBuilder(GRIP_CMD).inheritIO();  //.inheritIO().start()
-	Process gettingTargets;
+	boolean isEnabled = true;
 
 	public Vision() {
+		frame = NIVision.imaqCreateImage(ImageType.IMAGE_RGB, 0);
+		binaryFrame = NIVision.imaqCreateImage(ImageType.IMAGE_U8, 0);
+
+		session = NIVision.IMAQdxOpenCamera("cam0", NIVision.IMAQdxCameraControlMode.CameraControlModeController);
+
+		NIVision.IMAQdxConfigureGrab(session);
+
+		startLookingForTarget();
 	}
-	
-	public void init() {
-		startGripCommand();
-	}
-	
-	/**
-	 * Starting the grip command and assign to a Process to allow it to be destroyed.
-	 */
-	public void startGripCommand(){
-		try {
-			gettingTargets = builder.start();
-		} catch (Exception e) {
+
+	public void startLookingForTarget() {
+		NIVision.IMAQdxStartAcquisition(session);
+
+		CameraServer.getInstance().setImage(frame);
+
+		while (isEnabled) {
+
+			NIVision.IMAQdxGrab(session, frame, 1);
+			
+
 		}
+
 	}
-	
+
+	// private final static String GRIP_CMD = "/usr/local/frc/JRE/bin/java -jar
+	// /home/lvuser/grip.jar /home/lvuser/FindingTargets.grip";
+	// NetworkTable table = NetworkTable.getTable("GRIP/myContoursReport");
+
+	// ProcessBuilder builder = new ProcessBuilder(GRIP_CMD).inheritIO();
+	// //.inheritIO().start()
+	// Process gettingTargets;
+
+	// public void init() {
+	// startGripCommand();
+	// }
+	//
+	// /**
+	// * Starting the grip command and assign to a Process to allow it to be
+	// destroyed.
+	// */
+	// public void startGripCommand(){
+	// try {
+	// gettingTargets = builder.start();
+	// } catch (Exception e) {
+	// }
+	// }
+	//
+	// /**
+	// * You need the total destruction
+	// */
+	// public void destoryGripCommand(){
+	// gettingTargets.destroyForcibly();
+	// }
+
 	/**
-	 * You need the total destruction
-	 */
-	public void destoryGripCommand(){
-		gettingTargets.destroyForcibly();
-	}
-	
-	/**	
 	 * 
-	 * Given the arrays centerX and centerY from the network tables.
-	 * Will return the x, y coordinates that has the max width 
-	 * in which we want to shoot.
+	 * Given the arrays centerX and centerY from the network tables. Will return
+	 * the x, y coordinates that has the max width in which we want to shoot.
 	 * 
 	 * @return
 	 */
-	public double[] findMidXYCoords(){
-		double[] x = new double[0];
-		table.getNumberArray("centerX", x);
-		double[] y = new double[0];
-		table.getNumberArray("centerY", y);
-		double[] width = new double[0];
-		table.getNumberArray("centerY", width);
-		int index = maxIndex(width);
-		return new double[]{x[index], y[index]};
-	}
-	
+	// public double[] findMidXYCoords(){
+	// double[] x = new double[0];
+	// table.getNumberArray("centerX", x);
+	// double[] y = new double[0];
+	// table.getNumberArray("centerY", y);
+	// double[] width = new double[0];
+	// table.getNumberArray("centerY", width);
+	// int index = maxIndex(width);
+	// return new double[]{x[index], y[index]};
+	// }
+
 	/**
 	 * Returns index at which it has max double value in array input
+	 * 
 	 * @param input
 	 * @return
 	 */
-	private int maxIndex(double[] input){
+	private int maxIndex(double[] input) {
 		double max = 0;
 		int index = 0;
-		for(int i = 0; i < input.length; i++){
-			if(input[i] > max){
+		for (int i = 0; i < input.length; i++) {
+			if (input[i] > max) {
 				max = input[i];
 				index = i;
 			}
